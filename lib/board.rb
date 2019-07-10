19da50a35af28498dfe18fe12257f337185175cc
Jasmine Chen <jasmine1226@gmail.com>
2019-07-10 01:00:50 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,7 +12,12 @@ class Board
   end
 
   def display
-    puts self.cells
+    puts self.cells[0] + " | " + self.cells[1] + " | " +self.cells[2]
+    puts "-----------"
+    puts self.cells[3] + " | " + self.cells[4] + " | " +self.cells[5]
+    puts "-----------"
+    puts self.cells[6] + " | " + self.cells[7] + " | " +self.cells[8]
+
   end
 
   def position(input)
